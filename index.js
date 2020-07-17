79cac434e9ab5297d7dc185d5b706bdd16495921
Brandon Gallagher <brandonegallagher@gmail.com>
2020-07-17 14:52:10 -0400

    Switched ?help to .help and added URL shortener for direct link to torrent.

--- index.js
+++ index.js
@@ -8,29 +8,46 @@ Description: NodeJS Discord Bot that uses the 1337x API to grab relevant torrent
 //Config Stuff
 const config = require("./config.json");
 const token = config.token;
+let urlencode = require("urlencode");
+let fetch = require("node-fetch");
 
 //Discord Stuff
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
 //Command Prefix
-const prefix = "?";
+const prefix = ".";
 
 //1337x Torrent API
 const torrentApi = require("torrent-search-api");
 torrentApi.enableProvider("1337x");
 
 
-client.on("message", msg => {
-    if (!msg.content.startsWith(prefix) || msg.author.bot) return;
+client.on("ready", () => {
+    client.user.setPresence({
+        activity : {
+            name : "FOR COMMANDS DO: ?help"
+        },
+        status : "online"
+    })
+});
 
+client.on("message", msg => {
+    if (!msg.content.startsWith(prefix) || msg.author.bot) 
+    {
+        return;
+    }
     const args = msg.content.slice(prefix.length).trim().split(/ +/);
     const command = args.shift().toLowerCase();
+
+
 
     let query = "";
     args.map(string=> {
         query += " " + string;
     });
+
+    console.log(`${msg.author.username} used command ${query}`);
 
     if (command == "torrent")
     {
@@ -50,29 +67,73 @@ client.on("message", msg => {
                     .setFooter(Date())
             )
         });
-        
-        
     }
-    else if (command == "help")
+        // 
+
+    if (command == "help")
     {
-        msg.channel.send("``` ?torrent <torrent name> ```");
+        msg.channel.send("``` .torrent <torrent name> \n .torrent The Witcher 3 Wild Hunt```");
     }
+    
+    
 
 });
 
-async function grabTorrent(torrent)
+const grabTorrent = async (torrent)=>
 {
     let torrentObj;
     let magnet;
+    let shortened;
 
     await torrentApi.search(torrent)
         .then(e => {
             torrentObj = e[0]; //First object in the array of torrents
         });
     
-    magnet = await torrentApi.getMagnet(torrentObj);
-    return {"torrent" : torrentObj, "magnet" : magnet};
+    await torrentApi.getMagnet(torrentObj)
+        .then(mag => magnet = mag);
+
+    await shorten(magnet)
+        .then(short => shortened = short);
+    return {"torrent" : torrentObj, "magnet" : shortened};
 }
 
 client.login(token);
 
+
+async function shorten(magnet)
+{
+    let shortened;
+    try
+    {
+        let encoded = urlencode(magnet);
+        let query = await fetch(`http://mgnet.me/api/create?&format=json&opt=&m=${encoded}&_=1595006240839`, {
+            "headers": {
+              "accept": "text/javascript, application/javascript, application/ecmascript, application/x-ecmascript, */*; q=0.01",
+              "accept-language": "en-US,en;q=0.9",
+              "x-requested-with": "XMLHttpRequest",
+              "cookie": "NSHcookie=20111006b0a72d26c6f0003"
+            },
+            "referrer": "http://mgnet.me/",
+            "referrerPolicy": "no-referrer-when-downgrade",
+            "body": null,
+            "method": "GET",
+            "mode": "cors"
+          })
+          .then(res => {
+              return res.text();
+          })
+          .then(data => {
+              let obj = JSON.parse(data);
+              shortened = obj.shorturl;
+          });
+
+        return await Promise.resolve(shortened);
+    }
+    catch(err)
+    {
+        throw err;
+    }
+
+    
+}
