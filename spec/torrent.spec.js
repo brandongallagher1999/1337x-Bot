f0cf4f2bcf00208b1bae7a10ee8e260d679e5443
Brandon Gallagher <brandonegallagher@gmail.com>
2021-06-22 10:00:58 -0400

    Fixed test file and ready for proper CI.

--- spec/torrent.spec.js
+++ spec/torrent.spec.js
@@ -2,12 +2,21 @@ const {grabTorrents} = require("../dist/modules/torrent");
 
 jest.setTimeout(1000000);
 
-test("Testing torrent grabbing", async function() {
-
-    let dummyObject = await grabTorrents("Inception");
+const runTest = async () => {
     let res = await grabTorrents("Inception"); //first object in response
     console.log(res);
     expect(Object.keys(res[0])).toStrictEqual(["title", "time", "seeds", "peers", "size", "desc", "provider", "magnet", "number"]); //verifying schema
     expect(res.length).toBe(3); // 3 results
+}
 
+test("Testing torrent grabbing", async () => {
+    try
+    {
+        runTest();
+    } catch (err)
+    {
+        console.log(err);
+        runTest(); // Why am I calling this function again? Because the first grabTorrents() always fails due to some silly CloudFlare error.
+    }
 });
+
