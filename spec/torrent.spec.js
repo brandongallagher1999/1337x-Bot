67a8f65acd34c6ef8f874f8443124f181ecd168d
Brandon Gallagher <brandonegallagher@gmail.com>
2021-06-22 09:54:36 -0400

    CI test file change

--- spec/torrent.spec.js
+++ spec/torrent.spec.js
@@ -1,11 +1,13 @@
 const {grabTorrents} = require("../dist/modules/torrent");
 
-jest.setTimeout(10000);
+jest.setTimeout(1000000);
 
 test("Testing torrent grabbing", async function() {
+
+    let dummyObject = await grabTorrents("Inception");
     let res = await grabTorrents("Inception"); //first object in response
-    
-    expect(Object.keys(res[0])).toStrictEqual(["number", "title", "desc", "size", "seeds", "magnet"]); //verifying schema
+    console.log(res);
+    expect(Object.keys(res[0])).toStrictEqual(["title", "time", "seeds", "peers", "size", "desc", "provider", "magnet", "number"]); //verifying schema
     expect(res.length).toBe(3); // 3 results
 
 });
