384729e9de263723f0d48538bef0ca3c2ed0dba6
brandongallagher1999 <brandonegallagher@gmail.com>
2022-03-14 12:35:14 -0700

    Added a DockerHub Registry in Terraform for future image pulling in the CD pipeline.

---
+++ terraform/registry.tf
@@ -0,0 +1,6 @@
+resource "dockerhub_repository" "brandongallagher1999_1337x_bot" {
+  name             = "1337x-bot"
+  description      = "1337x-bot DockerHub Registry"
+  full_description = "1337x-bot DockerHub Registry"
+  namespace = "brandongallagher1999"
+}
